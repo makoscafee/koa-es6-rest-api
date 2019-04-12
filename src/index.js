7e92db01a48bb227951b4d438c78371568b1df76
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-04-12 11:19:44 +0300

    added logger and fix build script'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Koa from 'koa';
+import cors from '@koa/cors';
+import logger from 'koa-logger';
 
+// Start an app
 const app = new Koa();
+app.use(logger());
+app.use(cors());
+
+app.use((ctx, next) => {
+  return next();
+});
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
